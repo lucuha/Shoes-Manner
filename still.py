3be11f4134c4f75a236d98d3e2c2bcc9ac9df3ae
lucuha <ink_pastel@yahoo.co.jp>
2021-04-03 12:41:49 +0900

    Add files via upload

---
+++ still.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+#python3 still.py  --width=640 --height=480 --input-rate=1  --headless  csi://0 image/img_%04i.jpg
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+import sys
+
+import datetime
+
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Classify a live camera stream using an image recognition DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.imageNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--camera", type=str, default="0", help="index of the MIPI CSI camera to use (e.g. CSI camera 0)\nor for VL42 cameras, the /dev/video device to use.\nby default, MIPI CSI camera 0 will be used.")
+parser.add_argument("--width", type=int, default=1280, help="desired width of camera stream (default is 1280 pixels)")
+parser.add_argument("--height", type=int, default=720, help="desired height of camera stream (default is 720 pixels)")
+parser.add_argument('--headless', action='store_true', default=(), help="run without display")
+
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+
+# create video sources & outputs
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+
+
+# process frames until the user exits
+while True:
+	#sleep(1)
+	# capture the next image
+	img = input.Capture()
+
+	# render the image
+	output.Render(img)
+	print("save imame")
+	# exit on input/output EOS
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
